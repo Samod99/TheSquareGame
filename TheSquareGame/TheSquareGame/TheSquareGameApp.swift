411409177a39c6e9e31965b62575b80c44862bba
Samod99 <binushasamod@gmail.com>
2025-01-05 10:51:20 +0530

    Create 9 buttons with two colors and make them as a game (if user select two same coloured buttons score as 1)

---
+++ TheSquareGame/TheSquareGame/TheSquareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSquareGameApp.swift
+//  TheSquareGame
+//
+//  Created by Samod 037       on 2024-12-15.
+//
+
+import SwiftUI
+
+@main
+struct TheSquareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
